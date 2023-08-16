62b5895c54953147e0b88730d3da741635122e17
hariyebk <harunbekri6@gmail.com>
2023-08-16 09:43:39 +0300

    Updated Css module for Map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,5 @@
 import { useNavigate} from 'react-router-dom'
-import styles from './map.module.css'
+import styles from './Map.module.css'
 import {useEffect, useState} from "react"
 import Search from '../Search/Search'
 import { useCities } from '../../contexts/CityProvider'
